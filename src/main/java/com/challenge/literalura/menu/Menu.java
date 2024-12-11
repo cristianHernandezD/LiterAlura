eb0f0b1de2c3f64a14a3403feaa1674c00a369a8
Cristian Hernandez <cristiandhd@gmail.com>
2024-12-11 10:53:24 -0500

    Creacion repositorio

---
+++ src/main/java/com/challenge/literalura/menu/Menu.java
@@ -0,0 +1,23 @@
+package com.challenge.literalura.menu;
+
+public class Menu {
+    private String menu = """
+    1-Buscar libro por título
+    2-Listar libros registrados
+    3-Listar autores registrados
+    4-Listar autores vivos en un determinado año
+    5-Listar libros por idioma
+    0-Salir
+    
+    Elija una opción a través de su número: """;
+
+    private String bienvenida = "Bienvenidos a literAlura";
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public String getBienvenida() {
+        return bienvenida;
+    }
+}
